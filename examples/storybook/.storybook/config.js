856e06bb7b84da6e920a9a63814526b85236b79c
Youcef Mammar <youcef.mammar@algolia.com>
2019-02-08 19:51:15 +0100

    fix back storybook

--- examples/storybook/.storybook/config.js
+++ examples/storybook/.storybook/config.js
@@ -3,33 +3,7 @@ import { configure } from '@storybook/angular';
 const req = require.context('../src/stories/', true, /\.stories\.ts$/);
 
 function loadStories() {
-  [
-    './Breadcrumb.stories.ts',
-
-    // './ClearRefinements.stories.ts',
-    // './Configure.stories.ts',
-    // './CurrentRefinements.stories.ts',
-    // './CustomWidgets.stories.ts',
-    // './HierarchicalMenu.stories.ts',
-    // './Hits.stories.ts',
-    // './HitsPerPage.stories.ts',
-    // './InfiniteHits.stories.ts',
-
-    // './InstantSearch.stories.ts',
-    // './Menu.stories.ts',
-    // './NumericMenu.stories.ts',
-    // './NumericSelectors.stories.ts',
-    // './Pagination.stories.ts',
-    // './RangeInput.stories.ts',
-    // './RangeSlider.stories.ts',
-    // './RatingMenu.stories.ts',
-    // './RefinementList.stories.ts',
-    // './SearchBox.stories.ts',
-    // './SortBy.stories.ts',
-    // './Stats.stories.ts',
-    // './Toggle.stories.ts',
-  ].forEach(filename => req(filename));
-  // req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename));
 }
 
 import './styles.css';
